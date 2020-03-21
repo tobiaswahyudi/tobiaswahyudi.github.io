0519012cea37d254c9aac47ff2ba9436e1d1e302
Tobias Wahyudi <twahyudi@secretmission.io>
2020-03-21 12:35:29 -0400

    Setup gatsby-remark-images and remove gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,21 +24,20 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    {
       resolve: "gatsby-transformer-remark",
       options: {
-        plugins: ["gatsby-remark-autolink-headers"],
+        plugins: [
+          "gatsby-remark-autolink-headers",
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 200,
+            },
+          },
+        ],
         "excerpt_separator": `<!-- end -->`
       },
     },
