a722e7c352134a433b92ae711c0095b812a5d049
Tobias Wahyudi <twahyudi@secretmission.io>
2020-03-21 20:08:41 -0400

    Remove unused default components

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,25 @@
 import React from "react"
+import { Link } from "gatsby"
+import SEO from '../components/seo.js'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+const NotFoundPage = () => {
+  return (
+    <>
+      <div
+        style={{
+          paddingTop: '30vh',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexDirection: 'column'
+        }}
+      >
+        <SEO title="404: Not found" />
+        <h1>NOT FOUND</h1>
+        <p>You just hit a route that doesn&#39;t exist :( click <Link to="/">here</Link> to go to the homepage.</p>
+      </div>
+    </>
+  )
+}
 
 export default NotFoundPage
