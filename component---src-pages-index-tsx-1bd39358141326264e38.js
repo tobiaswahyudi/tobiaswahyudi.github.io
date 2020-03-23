9c7e6bd819111af6763864ed006d52677e4d7bc6
tobiaswahyudi <tobi8800@gmail.com>
2020-03-23 23:12:04 +0000

    Deploying to master from  - b378d5d4bcad2c38c85ab6f5ef2b57b4e43e16c2 🚀

---
+++ component---src-pages-index-tsx-1bd39358141326264e38.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{QeBL:function(e,t,n){"use strict";n.r(t),n.d(t,"query",(function(){return u}));var a=n("q1tI"),r=n.n(a),o=n("dmsj"),c=n("1XyS"),u="1585376260";t.default=function(e){var t=e.data;return r.a.createElement(r.a.Fragment,null,r.a.createElement(o.a,{title:"Home"}),r.a.createElement(c.default,{work:t.work.edges,projects:t.projects.edges}))}}}]);
+//# sourceMappingURL=component---src-pages-index-tsx-1bd39358141326264e38.js.map
