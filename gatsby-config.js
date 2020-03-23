765db639416da0dab967fa44ec9d16a8f8f1f1c8
Tobias Wahyudi <twahyudi@secretmission.io>
2020-03-23 18:03:53 -0400

    Setup typescript to help with graphql types

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-catch-links`,
+    `gatsby-plugin-typescript`,
     {
       resolve: `gatsby-plugin-prefetch-google-fonts`,
       options: {
