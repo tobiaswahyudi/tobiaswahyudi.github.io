5641ebdcab6358a5b9e71c46f366f37b5694c30b
tobiaswahyudi <tobi8800@gmail.com>
2020-04-05 15:49:35 -0400

    Create contact form components

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import * as yup from 'yup'
+import { Formik, FormikProps, ErrorMessage } from 'formik'
 
 import './index.scss'
 
@@ -18,6 +20,7 @@ const LandingPage: React.FC<LandingPageProps> = ({ work, projects }: LandingPage
       <LandingPageIntro />
       <Cards cards={work} title="Work Experience" />
       <Cards cards={projects} title="Projects" />
+      <ContactForm onSubmit={console.log} />
     </div>
   )
 }
@@ -82,6 +85,88 @@ export const Cards: React.FC<CardsProps> = ({ cards = [], title }: CardsProps) =
         </div>
         <ReadMore path='/' />
       </div>
+    </>
+  )
+}
+
+export interface FormFields {
+  name: string
+  email: string
+  message: string
+}
+
+interface ContactFormProps {
+  onSubmit: (data: FormFields) => void
+}
+
+export const ContactForm: React.FC<ContactFormProps> = ({
+  onSubmit
+}: ContactFormProps) => {
+
+  const validationSchema = yup.object().shape({
+    name: yup.string().required("Please provide your name."),
+    email: yup.string().email("Please provide a valid email.").required("Please provide your email."),
+    message: yup.string().required("Please leave a message.")
+  })
+
+  return (
+    <>
+      <div className="contact">
+        <h2>Contact Me</h2>
+        <div className="contact-form">
+          <Formik
+            initialValues={{ name: "", email: "", message: "" }}
+            validationSchema={validationSchema}
+            validateOnBlur={false}
+            validateOnChange={false}
+            validateOnMount={false}
+            onSubmit={onSubmit}
+          >
+            {(formik: FormikProps<FormFields>) => {
+
+              return (
+                <form onSubmit={formik.handleSubmit}>
+                  <div id="top">
+                    <div className="field-container">
+                      <input
+                        id="name"
+                        className={`overpass-light ${formik.errors.name ? 'has-errors' : ''}`}
+                        placeholder="Name and/or company"
+                        name="name"
+                        onChange={formik.handleChange}
+                      />
+                      <ErrorMessage name="name">{msg => <div className="form-error overpass-light">{msg}</div>}</ErrorMessage>
+                    </div>
+                    <div className="field-container">
+                      <input
+                        className={`overpass-light ${formik.errors.email ? 'has-errors' : ''}`}
+                        placeholder="Email"
+                        name="email"
+                        type="email"
+                        onChange={formik.handleChange}
+                      />
+                      <ErrorMessage name="email">{msg => <div className="form-error overpass-light">{msg}</div>}</ErrorMessage>
+                    </div>
+                  </div>
+                  <div id="middle">
+                    <textarea
+                      className={`overpass-light ${formik.errors.message ? 'has-errors' : ''}`}
+                      placeholder="Message"
+                      name="message"
+                      onChange={formik.handleChange}
+                    />
+                    <ErrorMessage name="message">{msg => <div className="form-error overpass-light">{msg}</div>}</ErrorMessage>
+                  </div>
+                  <div id="bottom">
+                    <input className="overpass-regular" id="send" type="submit" value="Send!" />
+                  </div>
+                </form>
+              )
+            }
+            }
+          </Formik>
+        </div>
+      </div >
     </>
   )
 }
