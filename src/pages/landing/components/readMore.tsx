a07ba6bd3925b5b0467f694eb23116c8439a4207
tobiaswahyudi <tobi8800@gmail.com>
2020-04-05 11:51:52 -0400

    Fix attribute warning in readMore svg

--- src/pages/landing/components/readMore.tsx
+++ src/pages/landing/components/readMore.tsx
@@ -14,7 +14,7 @@ const ReadMore: React.FC<ReadMoreProps> = ({ path }: ReadMoreProps) => {
         <div className="inner">
           <span>read more</span>
           <svg width="194" height="24" viewBox="0 0 194 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M0 11.6961H171.574H192M192 11.6961L179.745 1.07733M192 11.6961L179.745 23" stroke="black" stroke-width="2" />
+            <path d="M0 11.6961H171.574H192M192 11.6961L179.745 1.07733M192 11.6961L179.745 23" stroke="black" strokeWidth="2" />
           </svg>
         </div>
       </Link>
