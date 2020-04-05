6d3785931ab281828f83d4b34c0fb14111467fb2
tobiaswahyudi <tobi8800@gmail.com>
2020-04-05 22:33:23 +0000

    Deploying to master from  @ da0b82e6d2f6eb5dff4c295c30c21ec5e03c11e4 🚀

---
+++ component---src-pages-index-tsx-188d01ccbaecaa978a25.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{QeBL:function(e,t,n){"use strict";n.r(t),n.d(t,"query",(function(){return u}));var a=n("q1tI"),r=n.n(a),o=n("dmsj"),c=n("1XyS"),u="2504828434";t.default=function(e){var t=e.data;return r.a.createElement(r.a.Fragment,null,r.a.createElement(o.a,{title:"Home"}),r.a.createElement(c.default,{work:t.work.edges,projects:t.projects.edges}))}}}]);
+//# sourceMappingURL=component---src-pages-index-tsx-188d01ccbaecaa978a25.js.map
---
+++ styles-1b6673e2f384681afa82.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{aDpD:function(n,o,c){},k5KR:function(n,o,c){},nXc9:function(n,o,c){},t5Mx:function(n,o,c){}}]);
+//# sourceMappingURL=styles-1b6673e2f384681afa82.js.map
