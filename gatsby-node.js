1446afa21b0156a4d406880263950d2b37a24f72
tobiaswahyudi <tobi8800@gmail.com>
2020-05-03 18:18:57 -0400

    Run createPage for projects page

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,9 +5,14 @@
  */
 
 // You can delete this file if you're not using it
-exports.createPages = async ({ graphql, actions: { createPage } }, options) => {
-  createPage({
+exports.createPages = async ({ graphql, actions }, options) => {
+   
+  actions.createPage({
     path: `${options.baseUrl}`,
-    component: require.resolve(`./src/pages/index.tsx`)
+    component: require.resolve(`./src/pages/index.tsx`),
+  });
+  actions.createPage({
+    path: `${options.baseUrl}/projects`,
+    component: require.resolve(`./src/pages/projects/index.tsx`)
   });
 }
