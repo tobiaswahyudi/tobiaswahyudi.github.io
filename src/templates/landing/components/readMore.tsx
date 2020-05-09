8a0d920d3f48615d9e9bb08037c18193197fc49a
tobiaswahyudi <tobi8800@gmail.com>
2020-05-09 12:31:52 -0400

    Make read more component responsive

--- src/templates/landing/components/readMore.tsx
+++ src/templates/landing/components/readMore.tsx
@@ -13,7 +13,7 @@ const ReadMore: React.FC<ReadMoreProps> = ({ path }: ReadMoreProps) => {
       <Link to={path}>
         <div className="inner">
           <span>read more</span>
-          <svg width="194" height="24" viewBox="0 0 194 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg viewBox="0 0 194 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M0 11.6961H171.574H192M192 11.6961L179.745 1.07733M192 11.6961L179.745 23" stroke="black" strokeWidth="2" />
           </svg>
         </div>
