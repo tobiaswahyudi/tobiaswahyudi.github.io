e92879c9870acf0135339481a05769010799e545
Tobias Wahyudi <tobi8800@gmail.com>
2021-01-26 11:44:05 -0500

    Navbar redesign
    Mobile and desktop navbar is now the same top bar.
    
    Highlight resume button using a new gradient.

--- src/shared/components/navbar.tsx
+++ src/shared/components/navbar.tsx
@@ -1,10 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import './navbar.scss'
 import { Link } from 'gatsby';
-// @ts-ignore
-import Burger from '../../images/burger.svg'
-// @ts-ignore
-import Collapse from '../../images/collapse.svg'
 
 
 interface NavbarProps {
@@ -14,7 +10,6 @@ interface NavbarProps {
 
 const Navbar: React.FC<NavbarProps> = ({ showOnScroll = false, scrollTriggerVh = 0 }: NavbarProps) => {
   const [scrollNav, setScrollNav] = useState<boolean>(!showOnScroll)
-  const [showMobileNav, setShowMobileNav] = useState<boolean>(false)
 
   const onScroll = (event: Event) => {
     if (window.scrollY > (window.innerHeight * scrollTriggerVh / 100)) {
@@ -33,47 +28,25 @@ const Navbar: React.FC<NavbarProps> = ({ showOnScroll = false, scrollTriggerVh =
   })
 
   return (
-    <>
-      <nav className={scrollNav ? 'show' : 'hide'}>
-        <div id="title">
-        <Link to='/'><h2>Tobias Wahyudi</h2></Link>
-        </div>
-        <div className="nav-desktop">
-          <Link to='/'>home</Link>
-          <NavSeparator />
-          <a href='/Resume.pdf'>resume</a>
-          <NavSeparator />
-          <Link to='/projects'>projects</Link>
-          <NavSeparator />
-          <Link to='/#contact'>contact</Link>
-        </div>
-        <button className='nav-mobile-button' onClick={() => setShowMobileNav(!showMobileNav)}>
-          <Burger className={!showMobileNav ? 'show' : 'hide'}/>
-          <Collapse className={showMobileNav ? 'show' : 'hide'}/>
-        </button>
-      </nav>
-      <div className={`nav-mobile ${showMobileNav ? 'show' : 'hide'}`}>
-        <ul>
-          <li>
-            <Link to='/'>home</Link>
-          </li>
-          <li>
-            <a href='/Resume.pdf'>resume</a>
-          </li>
-          <li>
-            <Link to='/projects'>projects</Link>
-          </li>
-          <li>
-            <Link to='/#contact'>contact</Link>
-          </li>
-        </ul>
+    <nav className={scrollNav ? 'show' : 'hide'}>
+      <div id="title">
+        <Link to='/' className="regular-title"><h2>Tobias Wahyudi</h2></Link>
+        <Link to='/' className="small-title"><h2>TW</h2></Link>
       </div>
-    </>
+      <div className="nav-links">
+        <Link to='/'>home</Link>
+        <NavSeparator />
+        <Link to='/projects'>projects</Link>
+        <NavSeparator />
+        <Link to='/#contact'>contact</Link>
+        <a href='/Resume.pdf' className="resume">resume</a>
+      </div>
+    </nav>
   )
 }
 
 const NavSeparator: React.FC = () => {
-  return <div className="sep">|</div>
+  return <span className="sep">|</span>
 }
 
 export default Navbar;
