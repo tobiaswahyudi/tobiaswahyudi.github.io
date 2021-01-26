fc64c0ebc1be5f54a30f679e2f329e88b95ce50c
Tobias Wahyudi <tobi8800@gmail.com>
2021-01-26 05:50:39 -0500

    Allow image components to be set maxHeight

--- src/shared/components/Image.tsx
+++ src/shared/components/Image.tsx
@@ -3,19 +3,45 @@ import GatsbyImage from 'gatsby-image'
 import { SharpImage } from '../types'
 
 interface ImageProps {
-  image: SharpImage
+  image: SharpImage,
+  heightRem?: number,
+  widthRem?: number
 }
 
-const Image: React.FC<ImageProps> = ({ image }: ImageProps) => {
+const Image: React.FC<ImageProps> = ({ image, heightRem, widthRem }: ImageProps) => {
   if (image.childImageSharp && image.childImageSharp.fluid) {
-    return <GatsbyImage fluid={image.childImageSharp.fluid} />;
+
+    const aspectRatio = image.childImageSharp.fluid.aspectRatio;
+
+    return <GatsbyImage fluid={image.childImageSharp.fluid}
+      style={{
+        ...(heightRem &&
+          { maxWidth: (aspectRatio * heightRem) + 'rem' }
+        ),
+        ...(widthRem &&
+          { width: widthRem + 'rem', height: (widthRem / aspectRatio) + 'rem' }
+        )
+      }}
+      imgStyle={{
+        ...(widthRem &&
+          { width: widthRem + 'rem', height: (widthRem / aspectRatio) + 'rem' }
+        )
+      }}
+    />;
   }
 
   if (image.childImageSharp && image.childImageSharp.fixed) {
     return <GatsbyImage fixed={image.childImageSharp.fixed} />;
   }
 
-  return <img src={image.publicURL} />;
+  return <img src={image.publicURL} style={{
+    ...(heightRem &&
+      { height: heightRem + 'rem' }
+    ),
+    ...(widthRem &&
+      { width: widthRem + 'rem' }
+    )
+  }}/>;
 };
 
 export default Image;
