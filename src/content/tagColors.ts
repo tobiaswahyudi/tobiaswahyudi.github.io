480e96079263a7655c10c756513e3ea77cbd2989
Tobias Wahyudi <tobi8800@gmail.com>
2021-01-26 07:44:01 -0500

    Add colors for chip tags

---
+++ src/content/tagColors.ts
@@ -0,0 +1,27 @@
+interface TagColor {
+  text: string
+  hue: number
+}
+
+export const TagColors: TagColor[] = [
+  { text: "SPRING", hue: 137 },
+  { text: "REACT", hue: 187 },
+  { text: "ELECTRON", hue: 187 },
+  { text: "NODE", hue: 115 },
+  { text: "STORYBOOK", hue: 346 },
+  { text: "APOLLO", hue: 216 },
+  { text: "DJANGO", hue: 86 },
+  { text: "GRAPHENE", hue: 30 },
+  { text: "GATSBY", hue: 256 },
+  { text: "BOOTSTRAP", hue: 256 },
+  { text: "HEROKU", hue: 256 },
+  { text: "TYPESCRIPT", hue: 216 },
+  { text: "TENSORFLOW", hue: 30 },
+  { text: "NUMPY", hue: 187 },
+  { text: "SPARQL", hue: 216 },
+  { text: "NEXTJS", hue: 187 },
+  { text: "BULMA", hue: 162 },
+  { text: "PEERJS", hue: 0 },
+  { text: "ANGULAR", hue: 0 },
+  { text: "FIREBASE", hue: 37 }
+]
