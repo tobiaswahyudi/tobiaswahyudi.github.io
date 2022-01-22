5ed1872cc191afb4288cead8ce8ed46002c39f12
Tobias Wahyudi <tobi8800@gmail.com>
2022-01-21 22:14:19 -0500

    Scroll Controller

---
+++ js/scrollController.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded',() => {
+  const main = document.getElementsByTagName('main')[0];
+
+  console.log(main)
+
+  main.addEventListener('scroll', (e) => {
+    console.log(e.target.scrollTop);
+    e.target.style.marginLeft = (e.target.scrollTop * -2) + 'px'
+  })
+})
