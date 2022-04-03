e525934da2b5d4964f38065bcf4d298ec67bfdfd
Tobias Wahyudi <toby@layer6.ai>
2022-04-03 13:08:22 -0400

    Isometric matrix scrolling

--- js/scrollController.js
+++ js/scrollController.js
@@ -1,10 +1,43 @@
+const WIDTH_CONST = 0.39;
+const HEIGHT_CONST = -0.25;
+
+let WIDTH = 0;
+let HEIGHT = 0;
+
 document.addEventListener('DOMContentLoaded',() => {
   const main = document.getElementsByTagName('main')[0];
+  const mainContents = document.getElementById('main-contents');
 
-  console.log(main)
+  WIDTH = main.offsetWidth;
+  HEIGHT = main.offsetHeight;
 
-  main.addEventListener('scroll', (e) => {
+  console.log({WIDTH, HEIGHT})
+
+  console.log({main, mainContents})
+
+  const scroller = (e) => {
     console.log(e.target.scrollTop);
-    e.target.style.marginLeft = (e.target.scrollTop * -Math.sqrt(3)) + 'px'
-  })
+
+    const pos = e.target.scrollTop;
+
+    const x0 = WIDTH_CONST * WIDTH;
+    const y0 = HEIGHT_CONST * HEIGHT;
+
+    const dx = -1.422 * pos;
+    const dy = -pos;
+
+    mainContents.style.transform =
+    `matrix3d(
+      0.71, -0.5, 0.5, 0,
+      0.71, 0.5, -0.5, 0,
+      0, 0.71, 0.71, 0,
+      ${x0 + dx}, ${y0 + dy}, 0, 1
+    )`
+  }
+
+  main.addEventListener('scroll', scroller);
+
+  scroller({target: {scrollTop: 0}});
 })
+
+385, -252.5
